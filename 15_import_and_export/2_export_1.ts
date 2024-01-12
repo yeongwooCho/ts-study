a1730eebb7904c2f149837cd5c0e90c761a0a760
yeongwoo <jyy0223@naver.com>
2024-01-12 19:28:21 +0900

    [add] ES Module - export

---
+++ 15_import_and_export/2_export_1.ts
@@ -0,0 +1,27 @@
+/**
+ * Export
+ */
+
+export class IdolModel {
+    name: string;
+    age: number;
+
+    constructor(name: string, age: number) {
+        this.name = name;
+        this.age = age;
+    }
+}
+
+export const rose = new IdolModel('로제', 28);
+
+export const number = 999;
+
+export interface ICat {
+    name: string;
+    age: number;
+}
+
+export default {
+    name: '코팩',
+    age: 23,
+}
---
+++ 15_import_and_export/2_import_2.ts
@@ -0,0 +1,15 @@
+import {IdolModel, rose, number, ICat} from "./2_export_1";
+
+const iu = new IdolModel('아이유', 32);
+console.log(iu);
+
+console.log(iu);
+console.log(rose);
+console.log(number);
+
+const cat: ICat = {
+    name: '냥냥',
+    age: 28,
+};
+console.log(cat);
+
